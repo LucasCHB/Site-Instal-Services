39b561f1f5c89c353e617456a4436d6fa2318a75
TonNom <LucasCHB>
2025-08-08 12:02:14 +0200

    BUG sur legal.jsx (character invalid) ⚠  Non résolu

--- dev_Frontend/src/App.jsx
+++ dev_Frontend/src/App.jsx
@@ -3,6 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Homepage from './pages/Homepage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Actus from './pages/Actus';
+import Portfolio from './pages/Portfolio';
+import Services from './pages/Services';
+import Legal from './pages/Legal';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,29 +29,33 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <h1>Vite + React</h1>
+          <p>{message}</p>
+          <div className="card">
+            <button onClick={() => setCount(count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+          </div>
+        </header>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/actus" element={<Actus />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/legal" element={<Legal />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <h1>{message}</h1>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter> 
   )
 }
 
