8771acb2f5d8dd29fd2902a5f35a3bba3240656f
TonNom <LucasCHB>
2025-08-13 16:19:02 +0200

    -Ajout des pages de styles SCSS

--- dev_Frontend/src/pages/Homepage.jsx
+++ dev_Frontend/src/pages/Homepage.jsx
@@ -1,6 +1,6 @@
 export default function Homepage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-blue-100">
+    <div className="w-full min-h-screen flex items-center justify-center bg-blue-100">
       <h1 className="text-4xl font-bold text-blue-800">
         Page d'accueil OK 🚀
       </h1>
